4d5cf9c80fa298703f639cf28e22af8f99275ce3
ing <inyoman_gurnitha@yahoo.com>
2020-05-01 21:03:03 +0700

    02.8 Controllers

---
+++ controller/user.js
@@ -0,0 +1,4 @@
+exports.sayHi = (req, res) => {
+    res.json({ message: "<h2>Hello from controler</h2>" });
+};
+
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-// http://localhost:8000/api --> see app.js/routes middleware
-// app.get("/", (req, res) => {
-router.get("/", (req, res) => {
-    res.send("<h2>Hello from router</h2>");
-});
+// router.get("/", (req, res) => {
+//     res.send("<h2>Hello from router</h2>");
+// });
+
+const {sayHi} = require("../controller/user.js");
+
+router.get('', sayHi);
 
 module.exports = router;
